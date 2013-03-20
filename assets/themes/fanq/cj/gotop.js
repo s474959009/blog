249b35c0b917cc7bb15426f0ab28947a96acf802
s474959009 <naneitou@qq.com>
2013-03-20 15:52:50 +0800

    gotop

--- assets/themes/fanq/cj/gotop.js
+++ assets/themes/fanq/cj/gotop.js
@@ -1,26 +1,26 @@
-$(function(){
-    var topDistance = 450;//go-top距顶端距离
-    var showDistance = 1;//距离顶端多少距离开始显示go-top
-    var gotopCon = "<div class='go-top'><a href='#'><img src='images/gotop.gif' /></a></div>"
-    var thisTop = $(window).scrollTop() + topDistance;
-    $("#container").css("position","relative");
-    $("#container").append(gotopCon);
-    $(".go-top").css("top",thisTop);
-    if($(window).scrollTop() < showDistance){
-        $(".go-top").hide();
+(function goTopEx(){
+    var obj=document.getElementById("goTopBtn");
+    function getScrollTop(){
+        return document.documentElement.scrollTop+document.body.scrollTop;
+    }
+
+    function setScrollTop(value){
+        if (!document.documentElement.scrollTop){
+
+            document.body.scrollTop = value;
         }
-    $(window).scroll(function(){
-        thisTop = $(this).scrollTop() + topDistance;
-        $(".go-top").css("top",thisTop);
-        if($(this).scrollTop() < showDistance){
-            $(".go-top").fadeOut("fast");
-            }else{
-                $(".go-top").fadeIn("fast");
-                }
-        });
-    $("a",".go-top").click(function(){
-        $("html").animate({scrollTop:0},"fast");//IE,FF
-        $("body").animate({scrollTop:0},"fast");//Webkit
-        return false;
-        });
-    });
+        else {
+            document.documentElement.scrollTop=value;
+        }
+    }
+    window.onscroll=function(){
+        getScrollTop()>0?obj.style.display="":obj.style.display="none";
+    };
+    obj.onclick=function(){
+        var goTop=setInterval(scrollMove,10);
+        function scrollMove(){
+            setScrollTop(getScrollTop()/1.1);
+            if(getScrollTop()<1)clearInterval(goTop);
+        }
+    };
+})(); 
